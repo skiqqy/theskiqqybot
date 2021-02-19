04b136bcb601990895d139fc041e1ea1a51f427a
Stephen Cochrane <skippycochrane@gmail.com>
2021-02-19 23:11:47 +0200

    Small fix to start script, and removed random keys skeleton filewq

--- main.sh
+++ main.sh
@@ -2,12 +2,12 @@
 pip3 install --user -r ./deps.txt
 gen_key_config () {
 	rand_key=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
-	cat << EOF > keys
+	cat << EOF > assets/keys
 api_id=<id>
 api_hash=<hash>
 bot_token=<token>
 database_encryption_key=$rand_key
 EOF
 }
-[ ! -f keys ] && gen_key_config || echo "keys file found, skipping."
+[ ! -f ./assets/keys ] && gen_key_config || echo "keys file found, skipping."
 python3 src/main.py
